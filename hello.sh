18b2eada5d7f974a053b23ad2198f4f6e454aca6
3ctr1x <63297910+BainesySP@users.noreply.github.com>
2023-03-16 21:20:52 +0000

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 touch hello.txt
-echo "Hello World" > hello.txt
+echo "Your script has worked. The ducky payload has sucessfully deployed. It used curl to download the raw bash file from Github, executed it and then open the outputted .txt file and displayed it to you" > hello.txt
